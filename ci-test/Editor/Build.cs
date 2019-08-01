9052bcf6ec6fa3808ed1899872d606a2e813fbca
MizoTake <mizotake123@gmail.com>
2019-08-01 23:27:23 +0900

    update project constitution

---
+++ ci-test/Editor/Build.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+    static void Execute ()
+    {
+        BuildPipeline.BuildPlayer ("Assets/Scenes/SampleScene.unity", Application.dataPath.Replace("Assets", "Build/") +  EditorUserBuildSettings.activeBuildTarget, EditorUserBuildSettings.activeBuildTarget, BuildOptions.None);
+    }
+}
